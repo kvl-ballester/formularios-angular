9bbf825b147c1deeb68c4496c7be88640ec5db60
kvl-ballester <kvl.ballester@gmail.com>
2022-02-01 11:48:32 +0100

    done template form

--- src/app/template-forms/template-forms.component.ts
+++ src/app/template-forms/template-forms.component.ts
@@ -4,7 +4,7 @@ interface IRegisterForm {
   name: string;
   email: string;
   password: string;
-  reapeatPass: string;
+  repeatPass: string;
 }
 
 @Component({
@@ -18,7 +18,7 @@ export class TemplateFormsComponent implements OnInit {
     name: "",
     email: "",
     password: "",
-    reapeatPass: ""
+    repeatPass: ""
   }
 
   constructor() { }
@@ -27,7 +27,24 @@ export class TemplateFormsComponent implements OnInit {
   }
 
   submit() {
-    
+    // Llegados a este punto, ya hemos podido validar todo excepto las contraseñas y ya recibimos los datos
+    console.log("Datos de inicio de sesión");
+    console.log(this.register.name);
+    console.log(this.register.email);
+    console.log(this.register.password);
+    console.log(this.register.repeatPass);
+
+    // COntrolar si el password y el password verificado son iguales
+    if (this.register.password !== this.register.repeatPass) {
+      // Emitir alerta POR NO SER IGUALES Y NO DEJAR ENVIAR DATOS
+      console.log(
+        "Hay que introducir las dos contraseñas iguales para validarlo"
+      );
+      // Echar un mensaje de alerta
+      return;
+    }
+
+    // Con estos datos ya todo OK, los enviamos al servidor para comprobar si el login es OK o NO
   }
 
 }
